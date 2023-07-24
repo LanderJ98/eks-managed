8f8e13b68bbce180cb66c0537bcf035455db6380
Josie Lander <landerjosie@gmail.com>
2023-07-24 18:42:38 +0100

    DNS for EKS cluster

---
+++ external_dns.tf
@@ -0,0 +1,35 @@
+module "external_dns_controller" {
+  source           = "./modules/terraform-helm-release"
+  release_name     = var.external_dns_release_name
+  repository       = var.external_dns_repository
+  chart_name       = var.external_dns_chart_name
+  namespace        = var.external_dns_namespace
+  create_namespace = var.external_dns_create_namespace
+  set = [
+    {
+      name  = "rbac.apiVersion"
+      value = "v1"
+      type = "string"
+    },
+    {
+      name  = "serviceAccount.name"
+      value = "external-dns"
+      type = "string"
+    },
+    {
+      name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+      value = module.iam_eks_role.iam_role_arn
+      type = "string"
+    },
+    {
+      name = "txtOwnerId"
+      value = module.eks.cluster_name
+      type = "string"
+    },
+    {
+      name = "logLevel"
+      value = "debug"
+      type = "string"
+    }
+  ]
+}
